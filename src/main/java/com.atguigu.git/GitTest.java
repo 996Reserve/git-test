3ac912d35167f64f4da7fceb6d7a0df27bba894c
SiZe <1652020671@qq.com>
2022-05-18 09:20:38 +0800

    hot-fix commit1

--- src/main/java/com.atguigu.git/GitTest.java
+++ src/main/java/com.atguigu.git/GitTest.java
@@ -3,5 +3,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git266!");
+        System.out.println("hello git366!");
+        System.out.println("hello git466!");
     }
 }
